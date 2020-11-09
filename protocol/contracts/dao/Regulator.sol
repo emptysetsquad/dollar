07206d3be1a08ce0d1d139e4bdbfe39d90d05345
x230xme <mike.papageorge+gh2@gmail.com>
2020-11-09 17:43:11 +0100

    Fix for 6% happening during contraction
    It was possible for 6% to be applied during contraction, so the price param was added to limit, passed from growSupply and shrinkSupply, in order to differentiate this state in limit. Resisted eliminating limit altogether and moving the ternaries to grow/shrinkSupply for now.

--- protocol/contracts/dao/Regulator.sol
+++ protocol/contracts/dao/Regulator.sol
@@ -48,7 +48,7 @@ contract Regulator is Comptroller {
     }
 
     function shrinkSupply(Decimal.D256 memory price) private {
-        Decimal.D256 memory delta = limit(Decimal.one().sub(price));
+        Decimal.D256 memory delta = limit(Decimal.one().sub(price), price);
         uint256 newDebt = delta.mul(totalNet()).asUint256();
         increaseDebt(newDebt);
 
@@ -57,20 +57,26 @@ contract Regulator is Comptroller {
     }
 
     function growSupply(Decimal.D256 memory price) private {
-        Decimal.D256 memory delta = limit(price.sub(Decimal.one()));
+        Decimal.D256 memory delta = limit(price.sub(Decimal.one()), price);
         uint256 newSupply = delta.mul(totalNet()).asUint256();
         (uint256 newRedeemable, uint256 lessDebt, uint256 newBonded) = increaseSupply(newSupply);
         emit SupplyIncrease(epoch(), price.value, newRedeemable, lessDebt, newBonded);
     }
 
-    function limit(Decimal.D256 memory delta) private view returns (Decimal.D256 memory) {
+    function limit(Decimal.D256 memory delta, Decimal.D256 memory price) private view returns (Decimal.D256 memory) {
 
-        // check if we have new coupons to be added to the _state.balance.redeemable, if so then getCouponSupplyChangeLimit.
-        uint256 totalRedeemable = totalRedeemable();
-        uint256 totalCoupons = totalCoupons();
-        Decimal.D256 memory supplyChangeLimit = (totalRedeemable < totalCoupons) ? Constants.getCouponSupplyChangeLimit() : Constants.getSupplyChangeLimit();
+        if (price.greaterThan(Decimal.one())) {
+            uint256 totalRedeemable = totalRedeemable();
+            uint256 totalCoupons = totalCoupons();
+            Decimal.D256 memory supplyChangeLimit = (totalRedeemable < totalCoupons) ? Constants.getCouponSupplyChangeLimit() : Constants.getSupplyChangeLimit();
+        }
+
+        if (price.lessThan(Decimal.one())) {
+            Decimal.D256 memory supplyChangeLimit = Constants.getSupplyChangeLimit();
+        }
 
         return delta.greaterThan(supplyChangeLimit) ? supplyChangeLimit : delta;
+
     }
 
     function oracleCapture() private returns (Decimal.D256 memory) {
