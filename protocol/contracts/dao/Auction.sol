5f7ab94bef3e7e5f83b675b25606e13b8c4e3cc5
cinquemb <1519017+cinquemb@users.noreply.github.com>
2021-01-03 05:32:47 -0500

    fix: sort on distance not on struct

--- protocol/contracts/dao/Auction.sol
+++ protocol/contracts/dao/Auction.sol
@@ -38,12 +38,12 @@ contract Auction is Setters {
         uint256 i = left;
         uint256 j = right;
         if(i==j) return;
-        uint256 pivot = arr[uint256(left + (right - left) / 2)];
+        uint256 pivot = arr[uint256(left + (right - left) / 2)].distance;
         while (i <= j) {
-            while (arr[uint256(i)] < pivot) i++;
-            while (pivot < arr[uint256(j)]) j--;
+            while (arr[uint256(i)].distance < pivot) i++;
+            while (pivot < arr[uint256(j)].distance) j--;
             if (i <= j) {
-                (arr[uint256(i)], arr[uint256(j)]) = (arr[uint256(j)], arr[uint256(i)]);
+                (arr[uint256(i)]., arr[uint256(j)]) = (arr[uint256(j)], arr[uint256(i)]);
                 i++;
                 j--;
             }
